6f0ba0e18bc3bcd59722f570581ad98bfb2c26a9
Vlad <vladimir.rodriguez.mtz@gmail.com>
2019-11-29 17:34:03 -0600

    Compilacion android app con correo electronico.

---
+++ tabs_001/.history/src/app/servicios/amonestaciones.service_20191122152811.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from "@angular/fire/firestore";
+import { InquilinoInterface } from '../models/inquilino.interface'
+
+import { AmonestacionesInterface } from '../models/amonestacion.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AmonestacionesService {
+
+  private amonestacionesCollection: AngularFirestoreCollection<AmonestacionesInterface  >;
+  constructor(private db: AngularFirestore) {
+    this.amonestacionesCollection = db.collection('sirv_t_amonestaciones');
+   }
+
+
+   getBusquedaAmonestaciones(idInquilino:string) {
+    try {
+      console.log('Inicia consulta vista=>');
+      const snapshotResult = this.db.collection('sirv_t_amonestaciones', ref => ref
+      .where('idInquilino', '==', idInquilino).orderBy('fechaRegistro','asc'));
+      return snapshotResult;
+    } catch (error) {
+      console.log('Error al devolver los datos' + error);
+    }
+  }
+
+  getBusquedaAmonestacionesId(idAmonestacion:string) {
+    try {
+      console.log('Inicia consulta vista=>');
+      const snapshotResult = this.db.collection('sirv_t_amonestaciones').doc(idAmonestacion).ref.get();
+      return snapshotResult;
+    } catch (error) {
+      console.log('Error al devolver los datos' + error);
+    }
+  }
+
+  addAmonestaciones(amonestaciones: AmonestacionesInterface){
+    try {
+      this.amonestacionesCollection.ref.add(amonestaciones);
+    } catch (e) {
+    console.log('Error al insertar las amonestaciones'+e);
+    }
+    
+  }
+
+  updateAmonestaciones( id: string, amonestaciones: AmonestacionesInterface){
+    try {
+      return this.amonestacionesCollection.doc(id).update(amonestaciones);
+    } catch (e) {
+    console.log('Error update amonestaciones: '+e);
+    }
+    
+  }
+}
